e48d17af85376fb8c44b3997c6688ca50c72cab7
Thaís A <thais.antero@gmail.com>
2023-01-20 17:59:49 -0300

    config: Configura a gem swagger e documentação do endpoint POST '/imc'
    Para isso foi instalada a gem 'rswag' e abilitado o uso da gem
    'rack-cors'.
    Foi configurado o arquivo 'spec/swagger/api/v1/imc_spec.rb' para
    o usuário poder acessar o endpoint '/api-docs/index.html' para
    realizar requisições na rota POST '/imc' e visualizar as respostas.

--- Gemfile
+++ Gemfile
@@ -34,12 +34,15 @@ gem "bootsnap", require: false
 # gem "image_processing", "~> 1.2"
 
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
-# gem "rack-cors"
+gem "rack-cors"
+
+gem "rswag"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[mri mingw x64_mingw]
   gem "rspec-rails", "~> 6.0.0"
+  gem "rswag-specs"
   gem "rubocop-rails", require: false
   gem "rubocop-rspec", require: false
   gem "simplecov", require: false
---
+++ spec/swagger/api/v1/imc_spec.rb
@@ -0,0 +1,51 @@
+require 'swagger_helper'
+
+RSpec.describe '/api/v1/imc_controller', type: :request do
+  path "/api/v1/imc" do
+    post 'Calculate IMC' do
+      tags 'Imc'
+      produces 'application/json'
+      consumes 'application/json'
+      parameter name: 'Authorization', in: :header, type: :string, default: 'Bearer valid_token'
+      parameter name: :params, in: :body, schema: {
+        type: :object,
+        properties: {
+          height: {
+            type: :number, default: 1.70, description: 'Person height'
+          },
+          weight: {
+            type: :number, default: 70, description: 'Person weight'
+          },
+        },
+        required: ['height, weight']
+      }
+
+      response '200', :ok do
+        schema type: :object,
+          properties: {
+            imc: { type: :number, default: 24.22 },
+            obesity: { type: :string, default: '0' },
+            classification: { type: :string, default: 'Normal' }
+          }
+
+        run_test!
+      end
+
+      response '400', :bad_request do
+        schema type: :object,
+        properties: {
+          errors: { type: :string, default: "mensagem de erro de validação" }
+        }
+        run_test!
+      end
+
+      response '401', :unauthorized do
+        schema type: :object,
+        properties: {
+          errors: { type: :string, default: "Token inválido" }
+        }
+        run_test!
+      end
+    end
+  end 
+end
