f4e513c9926f3ab381e7d87166e0ad4ea3a72db8
Thaís A <thais.antero@gmail.com>
2023-01-20 13:31:54 -0300

    refactor: Melhora design dos método de classificação do imc
    Para tornar o código mais enxuto e legível, foram modificados os métodos
    para classificação do IMC.

--- app/models/imc.rb
+++ app/models/imc.rb
@@ -2,6 +2,14 @@ class Imc
   include ActiveModel::Validations
   extend ActiveModel::Translation
 
+  IMC_GROUPS = {
+    0...18.5 => {classification: "Magreza", obesity: "0"},
+    18.5...25 => {classification: "Normal", obesity: "0"},
+    25...30 => {classification: "Sobrepeso", obesity: "I"},
+    30...40 => {classification: "Obesidade", obesity: "II"},
+    40.. => {classification: "Obesidade Grave", obesity: "III"}
+  }
+
   attr_reader :height, :weight
 
   validates :height, :weight, presence: true
@@ -13,35 +21,21 @@ class Imc
     @weight = weight
   end
 
+  def value
+    (weight / height**2).round(1)
+  end
+
   def classification
-    if value < 18.5
-      "Magreza"
-    elsif value < 25
-      "Normal"
-    elsif value < 30
-      "Sobrepeso"
-    elsif value < 40
-      "Obesidade"
-    else
-      "Obesidade Grave"
-    end
+    group[:classification]
   end
 
   def obesity
-    if value < 18.5
-      "0"
-    elsif value < 25
-      "0"
-    elsif value < 30
-      "I"
-    elsif value < 40
-      "II"
-    else
-      "III"
-    end
+    group[:obesity]
   end
 
-  def value
-    (weight / height**2).round(1)
+  private
+
+  def group
+    IMC_GROUPS.find { |range, group| break group if range === value }
   end
 end
